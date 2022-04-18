9421711b10dcdfcea44c077cce8a5ddf98c20f33
EddyKay101 <edwardkay10@gmail.com>
2022-04-19 00:09:36 +0100

    Initial commit

---
+++ components/Layout.js
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+
+export default function Layout({ title, keywords, description, children }) {
+
+    const router = useRouter();
+    return (
+        <div>
+            <Head>
+                <title>{title}</title>
+                <meta name='description' content={description} />
+                <meta name='keywords' content={keywords} />
+            </Head>
+
+            <div className={styles.container}>
+                {children}
+            </div>
+        </div>
+    )
+}
+
+Layout.defaultProps = {
+    title: 'Scrutiny | Find the hottest parties',
+    description: 'Find the latest music critisism',
+    keywords: 'music, good music, dj'
+}
